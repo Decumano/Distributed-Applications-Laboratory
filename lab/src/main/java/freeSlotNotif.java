3e8704a2aa74da1bcc432d564c6c02dc89d16968
Decumano <marcgg171000@gmail.com>
2023-02-08 10:25:51 +0100

    Init of laboratory
    Initialization of distributed applications laboratory

---
+++ lab/src/main/java/freeSlotNotif.java
@@ -0,0 +1,15 @@
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+public class freeSlotNotif {
+    
+}
