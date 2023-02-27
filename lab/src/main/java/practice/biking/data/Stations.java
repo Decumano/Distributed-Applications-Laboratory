8dc95fb56a22e10b79354670e0301190a9f57640
Decumano <marcgg171000@gmail.com>
2023-02-27 18:01:09 +0100

    Mid implementation
    Half implementation of bicing get stations logic

--- lab/src/main/java/practice/biking/data/Stations.java
+++ lab/src/main/java/practice/biking/data/Stations.java
@@ -2,6 +2,16 @@ package practice.biking.data;
 
 import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.Path;
+
+@Path("/Stations")
 public class Stations {
     private List<Station> stations;
 
@@ -18,5 +28,16 @@ public class Stations {
 
     public void setStations(List<Station> stations) {
         this.stations = stations;
+    }
+
+    @GET
+    @Path("/GetAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Station> getStationsData()
+    {
+        Client client = ClientBuilder.newClient();
+        WebTarget bicingJSON = client.target("https://api.bsmsa.eu").path("ext/api/bsm/gbfs/v2/en/station_status");
+        stations = bicingJSON.request(MediaType.APPLICATION_JSON_TYPE).get(new GenericType<List<Station>>() {});
+        return stations;
     }
 }
