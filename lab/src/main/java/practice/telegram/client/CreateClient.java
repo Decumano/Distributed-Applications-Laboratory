dbe9a9f4bbe6c626d7babc590f90bee5077a2459
Edgar Espinos <edgar.espinos01@estudiant.upf.edu>
2023-03-14 09:27:11 +0100

    CovidInfo

--- lab/src/main/java/practice/telegram/client/CreateClient.java
+++ lab/src/main/java/practice/telegram/client/CreateClient.java
@@ -10,7 +10,7 @@ public class CreateClient {
 
 	public static void main(String[] args) {
 		long id = 5925337525L;
-		Message message = new Message(id, "Message");
+		Message message = new Message(id, "hi");
 		Client client = ClientBuilder.newClient();
 		WebTarget targetSendMessage = client.target("https://api.telegram.org").path("/bot5558130347:AAFAjEGRwEjmSnnZGbQU7ZmV-2ayjCM4jeU/sendMessage");
 		String response = targetSendMessage.request().post(Entity.entity(message, MediaType.APPLICATION_JSON_TYPE),
